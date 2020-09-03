b079e4d7fc35b0200b96c838be7688365b4861a7
Jeremias Osuna <jereosuna73@gmail.com>
2020-09-03 16:31:12 -0300

    Uso de axios. Async data.

--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,5 +1,54 @@
 <template>
-    <div class="container">
-        <h1>Detalle del articulo id: {{ $route.params.id }}</h1>
+  <div class="container">
+    <div class="card">
+      <div class="card-body">
+        <h1>{{ articulo.title }}</h1>
+        <small>{{ articulo.userId }} - {{ articulo.nombreAutor }}</small>
+        <p>{{ articulo.body }}</p>
+      </div>
     </div>
+  </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      articulo: "",
+      user: ""
+    };
+  },
+  async asyncData({
+    isDev,
+    route,
+    store,
+    env,
+    params,
+    query,
+    req,
+    res,
+    redirect,
+    error
+  }) {
+    try {
+      const res = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${params.id}`
+      );
+
+      const articulo = res.data;
+
+      const resUser = await axios.get(
+        `https://jsonplaceholder.typicode.com/users/${res.data.userId}`
+      );
+
+      articulo.nombreAutor = resUser.data.name;
+
+      return { articulo };
+    } catch (error) {
+      return { error: ReferenceError };
+    }
+  }
+};
+</script>
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,26 +1,34 @@
 <template>
   <div class="container mt-5">
-    <div class="row">
-      <div class="col-md-3" v-for="(item, index) in articulos" :key="index">
-        <h2>
-          <nuxt-link :to="`/blog/${item.id}`">{{ item.titulo }}</nuxt-link>
-        </h2>
+    <div class="card my-2" v-for="(item, index) in articulos" :key="index">
+      <div class="card-body">
+        <nuxt-link :to="`/blog/${item.id}`">
+          <h1>{{ item.title }}</h1>
+        </nuxt-link>
+        <p>{{ item.body }}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data() {
     return {
-      articulos: [
-        { id: 1, titulo: "Titulo #1" },
-        { id: 2, titulo: "Titulo #2" },
-        { id: 3, titulo: "Titulo #3" },
-        { id: 4, titulo: "Titulo #4" },
-      ],
+      articulos: []
     };
   },
+  async created() {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts?_limit=10"
+      );
+      this.articulos = response.data;
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
 </script>
